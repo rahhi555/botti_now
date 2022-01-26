f66815699a8fcd185411bdb33ca00c375299ee72
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-01-26 23:49:30 +0900

    Add: likeのシステムテスト追加

--- test/system/likes_test.rb
+++ test/system/likes_test.rb
@@ -1,11 +1,16 @@
 require "application_system_test_case"
+require 'debug'
 
 class LikesTest < ApplicationSystemTestCase
-  # test 'いいねマークをクリックすると、いいねされること' do
-  #   visit posts_url
-  #
-  #
-  #
-  #
-  # end
+  test 'いいねマークをクリックすると、いいねされること' do
+    visit posts_url
+
+    like_partial = find "#like_post_#{posts(:one).id}", visible: false
+
+    like_partial.has_css?('.bg-green-600')
+
+    like_partial.find_button.click
+    like_partial.has_css?('.bg-pink-500')
+
+  end
 end
