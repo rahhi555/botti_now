2d344166e583cb8b586d01a877991b41c11f4e61
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-01-27 23:50:49 +0900

    add

--- test/system/likes_test.rb
+++ test/system/likes_test.rb
@@ -1,16 +1,21 @@
 require "application_system_test_case"
-require 'debug'
 
 class LikesTest < ApplicationSystemTestCase
   test 'いいねマークをクリックすると、いいねされること' do
+    # 要素が複数あると被ってクリックできなくなるので片方削除
+    posts(:two).destroy
+
     visit posts_url
 
     like_partial = find "#like_post_#{posts(:one).id}", visible: false
 
     like_partial.has_css?('.bg-green-600')
+    like_partial.has_text?("いいね！#{posts(:one).likes.length}")
 
-    like_partial.find_button.click
-    like_partial.has_css?('.bg-pink-500')
-
+    assert_difference 'Like.count' do
+      like_partial.find_button.click
+      like_partial.has_css?('.bg-pink-500')
+      like_partial.has_text?("いいね！#{posts(:one).reload.likes.length}")
+    end
   end
 end
