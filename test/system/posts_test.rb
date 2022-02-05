e18d0229d26b8fac1257cfa4e0595b95a5edefc3
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-02-05 19:01:08 +0900

    Add: chaplus apiのsystemテスト追加

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -1,6 +1,15 @@
 require "application_system_test_case"
+require 'minitest/mock'
 
 class PostsTest < ApplicationSystemTestCase
+  include ActiveJob::TestHelper
+
+  DUMMY_JSON = {
+    'responses': [
+      { 'utterance': 'ダミーレスポンス' }
+    ]
+  }.to_json
+
   test 'ユーザーは新しいコメントを投稿すると、新たに画面上に表示される' do
     visit posts_url
 
@@ -11,31 +20,62 @@ class PostsTest < ApplicationSystemTestCase
 
     assert_text 'メッセージを入力してください'
 
-    fill_in 'post_message', with: 'テストメッセージ'
+    fill_in 'post_message', with: '好きな食べ物は何ですか？'
 
-    click_button 'つぶやく'
+    # ジョブが１つ登録されたことを確認(assertを同じブロックに入れないとジョブ登録の前に判定してしまいエラーになる)
+    assert_enqueued_jobs 1 do
+      click_button 'つぶやく'
 
-    assert_text 'テストメッセージ'
-    assert_no_text 'メッセージを入力してください'
+      assert_text 'うーんとね...'
+      assert_no_text 'メッセージを入力してください'
+    end
+
+    # APIレスポンスのモック作成
+    res = Minitest::Mock.new.expect(:body, DUMMY_JSON)
+    # APIリクエストメソッドをスタブ化し、上のモックを返すよう変更する
+    ChaplusApiJob.stub_any_instance :send_chaplus_api, res do
+      perform_enqueued_jobs
+    end
+
+    assert_no_text 'うーんとね...'
+    assert_text '好きな食べ物は何ですか？'
+    assert_text 'ダミーレスポンス'
+    # ジョブが実行されたため、エンキューにジョブが無いことを確認する
+    assert_no_enqueued_jobs
+    # ジョブが一つ実行されたことを確認する
+    assert_performed_jobs 1
   end
 
   test 'ユーザーは新しいコメントを投稿すると、別のユーザーの画面上に表示される' do
     visit posts_url
 
-    assert_no_text 'テストメッセージ'
+    assert_no_text '好きな食べ物は何ですか？'
+    assert_no_text 'ダミーレスポンス'
 
     within_session :another_user do
       visit posts_url
 
-      fill_in 'post_message', with: 'テストメッセージ'
+      fill_in 'post_message', with: '好きな食べ物は何ですか？'
 
-      click_button 'つぶやく'
+      assert_enqueued_jobs 1 do
+        click_button 'つぶやく'
+        assert_text 'うーんとね...'
+      end
 
-      assert_text 'テストメッセージ'
+      res = Minitest::Mock.new.expect(:body, DUMMY_JSON)
+      ChaplusApiJob.stub_any_instance(:send_chaplus_api, res) do
+        perform_enqueued_jobs
+      end
+
+      assert_no_text 'うーんとね...'
+      assert_text '好きな食べ物は何ですか？'
+      assert_text 'ダミーレスポンス'
+      assert_no_enqueued_jobs
+      assert_performed_jobs 1
     end
 
-    assert_text 'テストメッセージ'
-
+    assert_text '好きな食べ物は何ですか？'
+    assert_text 'ダミーレスポンス'
     close_session :another_user
   end
 
