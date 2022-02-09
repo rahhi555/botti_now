bf42e24cd0962c0ca680f0e45243668ce39d70d8
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-02-10 02:33:21 +0900

    Add: ランキングページの無限スクロール実装

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,10 +1,10 @@
 class RankingController < ApplicationController
   def index
-    @posts = Post.desc_likes_count.includes(:user).page(1)
+    @posts = Post.desc_likes_count.includes(:user).page(1).per(20)
   end
 
   def load
-    @posts = Post.desc_likes_count.includes(:user).page(params[:page])
+    @posts = Post.desc_likes_count.includes(:user).page(params[:page]).per(20)
     turbo_stream
   end
 end
--- app/javascript/controllers/paginate_controller.js
+++ app/javascript/controllers/paginate_controller.js
@@ -1,8 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
+import { useIntersection } from 'stimulus-use'
 
 // Connects to data-controller="paginate"
 export default class extends Controller {
-  static targets = ['nextPageLink']
+  static targets = ['nextPageLink', 'infiniteScroll']
 
   postRemoveSelf({ target }) {
     const parentNode = target.parentNode
@@ -12,10 +13,20 @@ export default class extends Controller {
       this.nextPageFetch()
     }
   }
-  
+
   nextPageFetch() {
     if (!this.hasNextPageLinkTarget) return
     
     this.nextPageLinkTarget.click()
+  }
+
+  infiniteScrollTargetConnected() {
+    this.unobserve = useIntersection(this, { element: this.infiniteScrollTarget })[1]
+  }
+
+  appear() {
+    this.unobserve()
+    this.infiniteScrollTarget.removeAttribute('data-paginate-target')
+    this.nextPageFetch()
   }
 }
