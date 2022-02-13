e018add2b7fda063a9b38d5d05417358812493f3
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-02-13 14:09:07 +0900

    Add: sidekiqのSSL設定変更

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
+SIDEKIQ_REDIS_CONFIGURATION = {
+  url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0'),
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+}.freeze
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
+  config.redis = SIDEKIQ_REDIS_CONFIGURATION
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
+  config.redis = SIDEKIQ_REDIS_CONFIGURATION
 end
